bfaf391b5ee37004f5ca4432700f29e7f940f4be
Andre <emmaumade1@gmail.com>
2023-06-01 18:40:06 +0100

    first

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,16 +6,23 @@ const HomePage = () => {
     <div className="home_main">
       <div className="home_sidenave">
         <div className="home_navs">
-          <div className="">
-            <div className="">
-              <img src={""} />
-              <p></p>
+          <div className="home_dasboard">
+            <div className="home_dasboard_case">
+              <img src={""} className="home_dasboard_img" />
+              <p className="home_dasboard_text">Dashboard</p>
+            </div>
+          </div>
+
+          <div className="home_dasboard">
+            <div className="home_dasboard_case">
+              <img src={""} className="home_dasboard_img" />
+              <p className="home_dasboard_text">Dashboard</p>
             </div>
           </div>
         </div>
         <img
           className="home_logo"
-          src={process.env.PUBLIC_URL + ".svg"}
+          src={process.env.PUBLIC_URL + "logo 2.svg"}
           alt="logo"
         />
       </div>
